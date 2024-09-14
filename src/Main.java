38e3ced5dc12c7b5f5a37d95f91320973ed48dbd
ELINAH-J <elinerono95@gmail.com>
2024-09-14 14:12:25 +0300

    stack trace activity 1

---
+++ src/Main.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class Main{
+    public static void main(String[] args) {
+        // Create a Scanner object for user input
+        Scanner scanner = new Scanner(System.in);
+
+        int a, b, c;
+        a=10;
+        b=0;
+        c=a/b;
+        System.out.println("the value of c =" +c);
+    }
+}
